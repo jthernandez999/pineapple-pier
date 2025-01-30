287030024f12f82e0a86ec74c535090c0b5b122c
Joe Hernandez <j.t.hernandez999@gmail.com>
2025-01-29 19:57:48 -0800

    debugging ShopifyMenuOperation

--- components/grid/three-items.tsx
+++ components/grid/three-items.tsx
@@ -44,7 +44,8 @@ function ThreeItemGridItem({
 export async function ThreeItemGrid() {
   // Collections that start with `hidden-*` are hidden from the search page.
   const homepageItems = await getCollectionProducts({
-    collection: 'hidden-homepage-featured-items'
+    // collection: 'hidden-homepage-featured-items'
+    collection: 'shop-new-arrivals'
   });
 
   if (!homepageItems[0] || !homepageItems[1] || !homepageItems[2]) return null;
