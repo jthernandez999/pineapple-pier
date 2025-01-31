13dc0fcb1c679e5fcee559916c7e2aebac753b23
Joe Hernandez <j.t.hernandez999@gmail.com>
2025-01-30 21:10:59 -0800

    wip

--- components/three-collections.tsx
+++ components/three-collections.tsx
@@ -14,12 +14,12 @@ interface CollectionImageProps {
 
 export default function ThreeImageCollections({ collectionImages }: CollectionImageProps) {
   return (
-    <div className="flex aspect-auto h-[50vh] w-[100vw] min-h-max flex-col justify-center gap-4 bg-white p-4 sm:aspect-square md:h-[100vh] md:flex-row">
+    <div className="flex aspect-auto h-screen flex-col justify-center gap-4 bg-white p-4 sm:aspect-square md:h-[100vh] md:flex-row">
       {collectionImages.map((image, index) => (
         <div key={index} className="aspect-w-4 aspect-h-5 relative h-full w-full min-h-max">
           <Link href={image.buttonLink} className="relative block h-[100%]">
             <Image
-              className='py-auto h-full w-full rounded-md shadow-md aspect-auto'
+              className='py-auto h-full w-full rounded-md shadow-md aspect-auto sm:aspect-square md:rounded-none md:aspect-square'
               src={image.image}
               alt={`Image ${index + 1}`}
               fill
