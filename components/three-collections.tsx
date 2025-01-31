54eda77ee36924384652297365862a3900adadc8
Joe Hernandez <j.t.hernandez999@gmail.com>
2025-01-30 20:58:34 -0800

    updating three-collections mobile responsiveness

--- components/three-collections.tsx
+++ components/three-collections.tsx
@@ -14,19 +14,19 @@ interface CollectionImageProps {
 
 export default function ThreeImageCollections({ collectionImages }: CollectionImageProps) {
   return (
-    <div className="flex h-[100vh] w-[100vw] flex-col justify-center gap-4 bg-white p-4 md:h-[100vh] md:flex-row">
+    <div className="flex aspect-auto h-[50vh] w-[100vw] min-h-max flex-col justify-center gap-4 bg-white p-4 sm:aspect-square md:h-[100vh] md:flex-row">
       {collectionImages.map((image, index) => (
-        <div key={index} className="aspect-w-4 aspect-h-5 relative h-full w-full">
+        <div key={index} className="aspect-w-4 aspect-h-5 relative h-full w-full min-h-max">
           <Link href={image.buttonLink} className="relative block h-[100%]">
             <Image
+              className='py-auto h-full w-full rounded-md shadow-md aspect-auto'
               src={image.image}
               alt={`Image ${index + 1}`}
               fill
               style={{
                 objectFit: 'cover',
-                objectPosition: 'center',
+                objectPosition: 'center'
               }}
-              className="h-full w-full rounded-md shadow-md"
             />
             <h3 className="absolute bottom-0 left-0 w-full bg-black bg-opacity-10 p-2 text-lg font-semibold text-white">
               {image.title}
