6a4e6c48cb8cf7fd4bf69d0f3f0f05673bb64607
Joe Hernandez <j.t.hernandez999@gmail.com>
2025-01-31 19:34:39 -0800

    fixed mobile carousel

--- components/carousel.tsx
+++ components/carousel.tsx
@@ -12,13 +12,12 @@ export async function Carousel() {
   const carouselProducts = [...products, ...products, ...products];
 
   return (
-    // <div className="h-full w-full overflow-x-auto pb-0 pt-0">
     <div className="h-full w-full overflow-x-auto pb-0 pt-0">
       <ul className="flex animate-carousel gap-4">
         {carouselProducts.map((product, i) => (
           <li
             key={`${product.handle}${i}`}
-            className="relative aspect-[2/3] h-[30vh] w-[20vw] max-w-[20vw] flex-none sm:h-[30vw] sm:w-[20vw] md:h-[30vw] md:w-[20vw] lg:h-[30vw] lg:w-[20vw] xl:h-[30vw] xl:w-[20vw] 2xl:h-[30vw] 2xl:w-[20vw]"
+            className="relative aspect-[2/3] w-[66.67vw] flex-none sm:aspect-[2/3] sm:w-[20vw] md:w-[20vw] lg:w-[20vw] xl:w-[20vw] 2xl:w-[20vw]"
           >
             <Link href={`/product/${product.handle}`} className="relative h-full w-full">
               <GridTileImage
@@ -30,7 +29,7 @@ export async function Carousel() {
                 }}
                 src={product.featuredImage?.url}
                 fill
-                sizes="100vw 30vw 20vw 15vw 10vw 5vw 2.5vw 1.25vw 1vw 0.5vw 0.25vw 0.125vw 0.0625vw 0.03125vw 0.015625vw 0.0078125vw" // 16 sizes
+                sizes="100vw, (min-width: 768px) 20vw" // adjust as needed
                 className="object-cover"
               />
             </Link>
