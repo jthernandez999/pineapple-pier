6d8046465a7d820ebd33ae7b51684d29162c4e96
Joe Hernandez <j.t.hernandez999@gmail.com>
2025-01-31 20:04:03 -0800

    fixed three collections responsiveness

--- components/three-collections.tsx
+++ components/three-collections.tsx
@@ -3,46 +3,46 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 interface CollectionImage {
-  image: string;
-  title: string;
-  buttonLink: string;
+   image: string;
+   title: string;
+   buttonLink: string;
 }
 
 interface CollectionImageProps {
-  collectionImages: CollectionImage[];
+   collectionImages: CollectionImage[];
 }
 
 export default function ThreeCollections({ collectionImages }: CollectionImageProps) {
-  return (
-    <div className="w-full overflow-hidden bg-white p-4 md:h-screen md:flex-row">
-      <div className="flex flex-col gap-4">
-        {collectionImages.map((image, index) => {
-          // Determine the object position based on the image's position in the array
-          const objectPosition =
-            index === 0
-              ? 'object-top'
-              : index === collectionImages.length - 1
-                ? 'object-bottom'
-                : 'object-center';
+   return (
+      <div className="w-full overflow-hidden bg-white p-4 md:h-screen">
+         {/* Use flex-col on mobile and flex-row on desktop */}
+         <div className="flex flex-col gap-4 md:flex-row">
+            {collectionImages.map((image, index) => {
+               const objectPosition =
+                  index === 0
+                     ? 'object-top'
+                     : index === collectionImages.length - 1
+                       ? 'object-bottom'
+                       : 'object-center';
 
-          return (
-            <div key={index} className="md:aspect-w-4 md:aspect-h-5 relative aspect-square w-full">
-              <Link href={image.buttonLink} className="relative block h-full">
-                <Image
-                  className={`object-cover ${objectPosition} h-full w-full rounded-md shadow-md sm:object-cover md:rounded-none md:object-cover`}
-                  src={image.image}
-                  alt={`Image ${index + 1}`}
-                  fill
-                />
-
-                <h3 className="absolute bottom-0 left-0 w-full bg-black bg-opacity-10 p-2 text-lg font-semibold text-white">
-                  {image.title}
-                </h3>
-              </Link>
-            </div>
-          );
-        })}
+               return (
+                  // Force a 2:3 ratio using aspect-[2/3] and assign each image 100% width on mobile and 1/3 on desktop
+                  <div key={index} className="relative aspect-[2/3] w-full md:w-1/3">
+                     <Link href={image.buttonLink} className="relative block h-full">
+                        <Image
+                           className={`object-cover ${objectPosition} h-full w-full rounded-md shadow-md`}
+                           src={image.image}
+                           alt={`Image ${index + 1}`}
+                           fill
+                        />
+                        <h3 className="absolute bottom-0 left-0 w-full bg-black bg-opacity-10 p-2 text-lg font-semibold text-white">
+                           {image.title}
+                        </h3>
+                     </Link>
+                  </div>
+               );
+            })}
+         </div>
       </div>
-    </div>
-  );
+   );
 }
