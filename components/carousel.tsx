0ec65474df28e7d767b84415d14ea0ed8c2d68c7
Joe Hernandez <j.t.hernandez999@gmail.com>
2025-01-31 19:24:54 -0800

    wip carousel mobile

--- components/carousel.tsx
+++ components/carousel.tsx
@@ -18,7 +18,7 @@ export async function Carousel() {
         {carouselProducts.map((product, i) => (
           <li
             key={`${product.handle}${i}`}
-            className="relative aspect-[2/3] w-[30vw] max-w-[20vw] flex-none"
+            className="relative aspect-[2/3] h-[30vh] w-[20vw] max-w-[20vw] flex-none sm:h-[30vw] sm:w-[20vw] md:h-[30vw] md:w-[20vw] lg:h-[30vw] lg:w-[20vw] xl:h-[30vw] xl:w-[20vw] 2xl:h-[30vw] 2xl:w-[20vw]"
           >
             <Link href={`/product/${product.handle}`} className="relative h-full w-full">
               <GridTileImage
@@ -30,7 +30,7 @@ export async function Carousel() {
                 }}
                 src={product.featuredImage?.url}
                 fill
-                sizes="(min-width: 1024px) 25vw, (min-width: 768px) 33vw, 50vw"
+                sizes="100vw 30vw 20vw 15vw 10vw 5vw 2.5vw 1.25vw 1vw 0.5vw 0.25vw 0.125vw 0.0625vw 0.03125vw 0.015625vw 0.0078125vw" // 16 sizes
                 className="object-cover"
               />
             </Link>
