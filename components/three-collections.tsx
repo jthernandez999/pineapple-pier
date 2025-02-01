31d0139f7e4eb2256d21e634f1bff76d624880f6
Joe Hernandez <j.t.hernandez999@gmail.com>
2025-01-31 19:39:53 -0800

    wip three collections

--- components/three-collections.tsx
+++ components/three-collections.tsx
@@ -14,23 +14,35 @@ interface CollectionImageProps {
 
 export default function ThreeCollections({ collectionImages }: CollectionImageProps) {
   return (
-    <div className="flex h-screen w-full flex-col justify-center gap-4 overflow-hidden bg-white p-4 sm:aspect-square md:h-screen md:flex-row">
-      {collectionImages.map((image, index) => (
-        <div key={index} className="aspect-w-4 aspect-h-5 relative h-full min-h-max w-full">
-          <Link href={image.buttonLink} className="relative block h-[100%]">
-            <Image
-              className="xs:object-cover h-full w-full rounded-md object-cover shadow-md sm:object-cover md:rounded-none md:object-cover md:object-center"
-              src={image.image}
-              alt={`Image ${index + 1}`}
-              fill
-            />
+    <div className="w-full overflow-hidden bg-white p-4 md:h-screen md:flex-row">
+      <div className="flex flex-col gap-4">
+        {collectionImages.map((image, index) => {
+          // Determine the object position based on the image's position in the array
+          const objectPosition =
+            index === 0
+              ? 'object-top'
+              : index === collectionImages.length - 1
+                ? 'object-bottom'
+                : 'object-center';
 
-            <h3 className="absolute bottom-0 left-0 w-full bg-black bg-opacity-10 p-2 text-lg font-semibold text-white">
-              {image.title}
-            </h3>
-          </Link>
-        </div>
-      ))}
+          return (
+            <div key={index} className="md:aspect-w-4 md:aspect-h-5 relative aspect-square w-full">
+              <Link href={image.buttonLink} className="relative block h-full">
+                <Image
+                  className={`object-cover ${objectPosition} h-full w-full rounded-md shadow-md sm:object-cover md:rounded-none md:object-cover`}
+                  src={image.image}
+                  alt={`Image ${index + 1}`}
+                  fill
+                />
+
+                <h3 className="absolute bottom-0 left-0 w-full bg-black bg-opacity-10 p-2 text-lg font-semibold text-white">
+                  {image.title}
+                </h3>
+              </Link>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
