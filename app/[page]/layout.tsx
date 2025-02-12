18ed1f02b9c69b0bf911a24df55610f246e9269a
jthernandez999 <j.t.hernandez999@gmail.com>
2025-02-12 13:30:54 -0800

    added vercel speed-insights

--- app/[page]/layout.tsx
+++ app/[page]/layout.tsx
@@ -1,10 +1,14 @@
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import Footer from 'components/layout/footer';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
    return (
       <>
          <div className="w-full">
-            <div className="mx-8 max-w-2xl py-20 sm:mx-auto">{children}</div>
+            <div className="mx-8 max-w-2xl py-20 sm:mx-auto">
+               {children}
+               <SpeedInsights />
+            </div>
          </div>
          <Footer />
       </>
