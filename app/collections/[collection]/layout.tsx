e01c5120185d159d37282c442e52aba20b0c64bb
jthernandez999 <j.t.hernandez999@gmail.com>
2025-02-13 15:18:15 -0800

    added footer on collection page

---
+++ app/collections/[collection]/layout.tsx
@@ -0,0 +1,13 @@
+// app/collections/[collection]/layout.tsx
+import React from 'react';
+
+import Footer from '../../../components/layout/footer';
+
+export default function CollectionsLayout({ children }: { children: React.ReactNode }) {
+   return (
+      <>
+         <main>{children}</main>
+         <Footer />
+      </>
+   );
+}
