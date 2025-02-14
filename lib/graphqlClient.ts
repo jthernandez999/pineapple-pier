784c3a9e0404bb15460cd1fd81abfee685f983a9
jthernandez999 <j.t.hernandez999@gmail.com>
2025-02-13 16:55:21 -0800

    addd a graphql client

---
+++ lib/graphqlClient.ts
@@ -0,0 +1,24 @@
+// lib/graphqlClient.ts
+export async function graphQLClient(query: string, variables: any) {
+   const res = await fetch(process.env.NEXT_PUBLIC_SHOPIFY_GRAPHQL_ENDPOINT as string, {
+      method: 'POST',
+      headers: {
+         'Content-Type': 'application/json',
+         'X-Shopify-Storefront-Access-Token': process.env
+            .NEXT_PUBLIC_SHOPIFY_STOREFRONT_TOKEN as string
+      },
+      body: JSON.stringify({ query, variables })
+   });
+
+   if (!res.ok) {
+      // Handle HTTP errors
+      throw new Error(`HTTP error: ${res.statusText}`);
+   }
+
+   const json = await res.json();
+   if (json.errors) {
+      // Handle GraphQL errors
+      throw new Error(`GraphQL error: ${JSON.stringify(json.errors)}`);
+   }
+   return json.data;
+}
