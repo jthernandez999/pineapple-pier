1a1aa029754519ec93b5eba980ba9fa720a0b8c8
Joe Hernandez <j.t.hernandez999@gmail.com>
2025-02-14 22:29:02 -0800

    wip on the homepage hero

--- components/hero.tsx
+++ components/hero.tsx
@@ -62,97 +62,101 @@ export default function HeroBanner({ banners, interval = 4000 }: BannerProps) {
    };
 
    return (
-      <section
-         className="relative h-[calc(100vh-64px)] w-full overflow-hidden md:h-[calc(100vh-80px)] lg:h-[calc(100vh-96px)] xl:h-[calc(100vh-112px)] 2xl:h-[calc(100vh-128px)]"
-         ref={bannerRef}
-         onTouchStart={handleTouchStart}
-         onTouchMove={handleTouchMove}
-         onTouchEnd={handleTouchEnd}
-         onMouseDown={handleTouchStart}
-         onMouseMove={handleTouchMove}
-         onMouseUp={handleTouchEnd}
-         onMouseLeave={handleTouchEnd}
-      >
-         {banners.map((banner, index) => (
-            <div
-               key={index}
-               className={`absolute inset-0 transition-opacity duration-1000 ease-in-out ${
-                  index === currentBanner ? 'opacity-100' : 'opacity-0'
-               }`}
-               style={{ display: 'grid', placeItems: 'center' }}
-            >
-               {/* Desktop Image */}
-               <div className="hidden md:block">
-                  <Image
-                     src={banner.image}
-                     alt={banner.title || 'Banner'}
-                     priority={index === 0}
-                     fill
-                     style={{
-                        objectFit: 'cover',
-                        objectPosition: 'center'
-                     }}
-                     onLoad={() => setIsLoading(false)}
-                     // Apply the custom scaling transition:
-                     className={`${
-                        isLoading ? 'scale-110' : 'scale-100'
-                     } duration-1200 ease-custom transition-transform`}
-
-                     // className={` ${
-                     //    isLoading ? 'scale-110 blur-2xl' : 'scale-100 blur-none'
-                     // } duration-1200 ease-custom transition-transform hover:scale-105`}
-                  />
-               </div>
-               {/* Mobile Image */}
-               <div className="block h-[20vh] w-screen object-contain md:hidden">
-                  <Image
-                     src={banner.mobileImage || banner.image}
-                     alt={banner.title || 'Banner'}
-                     priority={index === 0}
-                     fill
-                     style={{
-                        objectFit: 'cover',
-                        objectPosition: 'center'
-                     }}
-                     onLoad={() => setIsLoading(false)}
-                     className={`${
-                        isLoading ? 'scale-110' : 'scale-100'
-                     } duration-1200 ease-custom transition-transform`}
-                  />
-               </div>
-
-               <div className="bg-black/01 absolute inset-0"></div>
-
-               <div className="relative z-10 p-4 text-center text-white">
-                  {banner.title && (
-                     <h2 className="text-xl font-bold md:text-3xl">{banner.title}</h2>
-                  )}
-                  {banner.description && <p className="text-sm md:text-lg">{banner.description}</p>}
-                  {banner.buttonText && banner.buttonLink && (
-                     <Link
-                        href={banner.buttonLink}
-                        className="ease-custom mt-4 inline-block bg-white px-6 py-2 text-sm font-medium text-black transition-all duration-1000 ease-in-out hover:scale-95"
-                     >
-                        {banner.buttonText}
-                     </Link>
-                  )}
-               </div>
-            </div>
-         ))}
-         {/* arrows to navigate between banners */}
-         <div className="absolute bottom-4 right-4 flex gap-3">
-            {banners.map((_, index) => (
-               <button
+      <div className="relative mt-0 h-full w-full p-4 pb-0 pt-0 md:h-screen">
+         <section
+            className="relative h-[calc(100vh-64px)] w-full overflow-hidden md:h-[calc(100vh-80px)] lg:h-[calc(100vh-96px)] xl:h-[calc(100vh-112px)] 2xl:h-[calc(100vh-128px)]"
+            ref={bannerRef}
+            onTouchStart={handleTouchStart}
+            onTouchMove={handleTouchMove}
+            onTouchEnd={handleTouchEnd}
+            onMouseDown={handleTouchStart}
+            onMouseMove={handleTouchMove}
+            onMouseUp={handleTouchEnd}
+            onMouseLeave={handleTouchEnd}
+         >
+            {banners.map((banner, index) => (
+               <div
                   key={index}
-                  onClick={() => setCurrentBanner(index)}
-                  className={`h-3 w-3 ${
-                     index === currentBanner
-                        ? 'rounded-lg bg-white shadow-lg'
-                        : 'bg-gray-400 hover:bg-white'
-                  } transition-transform duration-300`}
-               ></button>
+                  className={`absolute inset-0 transition-opacity duration-1000 ease-in-out ${
+                     index === currentBanner ? 'opacity-100' : 'opacity-0'
+                  }`}
+                  style={{ display: 'grid', placeItems: 'center' }}
+               >
+                  {/* Desktop Image */}
+                  <div className="hidden md:block">
+                     <Image
+                        src={banner.image}
+                        alt={banner.title || 'Banner'}
+                        priority={index === 0}
+                        fill
+                        style={{
+                           objectFit: 'cover',
+                           objectPosition: 'center'
+                        }}
+                        onLoad={() => setIsLoading(false)}
+                        // Apply the custom scaling transition:
+                        className={`${
+                           isLoading ? 'scale-110' : 'scale-100'
+                        } duration-1200 ease-custom transition-transform`}
+
+                        // className={` ${
+                        //    isLoading ? 'scale-110 blur-2xl' : 'scale-100 blur-none'
+                        // } duration-1200 ease-custom transition-transform hover:scale-105`}
+                     />
+                  </div>
+                  {/* Mobile Image */}
+                  <div className="scroll m-auto aspect-square md:hidden">
+                     <Image
+                        src={banner.mobileImage || banner.image}
+                        alt={banner.title || 'Banner'}
+                        priority={index === 0}
+                        fill
+                        style={{
+                           objectFit: 'cover',
+                           objectPosition: 'center'
+                        }}
+                        onLoad={() => setIsLoading(false)}
+                        className={`${
+                           isLoading ? 'scale-110' : 'scale-100'
+                        } duration-1200 ease-custom transition-transform`}
+                     />
+                  </div>
+
+                  <div className="bg-black/01 absolute inset-0"></div>
+
+                  <div className="relative z-10 p-4 text-center text-white">
+                     {banner.title && (
+                        <h2 className="text-xl font-bold md:text-3xl">{banner.title}</h2>
+                     )}
+                     {banner.description && (
+                        <p className="text-sm md:text-lg">{banner.description}</p>
+                     )}
+                     {banner.buttonText && banner.buttonLink && (
+                        <Link
+                           href={banner.buttonLink}
+                           className="ease-custom mt-4 inline-block bg-white px-6 py-2 text-sm font-medium text-black transition-all duration-1000 ease-in-out hover:scale-95"
+                        >
+                           {banner.buttonText}
+                        </Link>
+                     )}
+                  </div>
+               </div>
             ))}
-         </div>
-      </section>
+            {/* arrows to navigate between banners */}
+            <div className="absolute bottom-4 right-4 flex gap-3">
+               {banners.map((_, index) => (
+                  <button
+                     key={index}
+                     onClick={() => setCurrentBanner(index)}
+                     className={`h-3 w-3 ${
+                        index === currentBanner
+                           ? 'rounded-lg bg-white shadow-lg'
+                           : 'bg-gray-400 hover:bg-white'
+                     } transition-transform duration-300`}
+                  ></button>
+               ))}
+            </div>
+         </section>
+      </div>
    );
 }
