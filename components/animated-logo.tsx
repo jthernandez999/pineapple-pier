64d42cde91a60391b7b913865b0e730546a32c0e
Joe Hernandez <j.t.hernandez999@gmail.com>
2025-02-15 08:11:16 -0800

    debugginngg naaavvv!!!!

--- components/animated-logo.tsx
+++ components/animated-logo.tsx
@@ -8,14 +8,14 @@ import { useEffect, useState } from 'react';
 export default function AnimatedLogo() {
    const pathname = usePathname();
    const isHome = pathname === '/';
-   const scrolled = useScrolled(999);
+   const scrolled = useScrolled(100);
 
    if (isHome) {
       if (!scrolled) {
          // On the homepage when not scrolled: display the white logo overlay.
-         // Rendered inside a relatively positioned hero container.
+         // This should be rendered inside a relatively positioned hero container.
          return (
-            <div className="z-999 pointer-events-none absolute left-1/2 top-1/4 h-[60vh] w-[80vw] -translate-x-1/2 transform md:w-[999vw]">
+            <div className="pointer-events-none absolute left-1/2 top-1/4 z-[999] h-[60vh] w-[80vw] -translate-x-1/2 transform md:w-[100vw]">
                <Image
                   src="/dj_white_logo.png"
                   alt="DJ White Logo"
@@ -30,7 +30,7 @@ export default function AnimatedLogo() {
          return (
             <Link
                href="/"
-               className="z-999 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform md:left-0 md:ml-4 md:-translate-x-0"
+               className="absolute left-1/2 top-1/2 z-[999] -translate-x-1/2 -translate-y-1/2 transform md:left-0 md:ml-4 md:-translate-x-0"
             >
                <div className="relative h-64 w-64">
                   <Image
@@ -48,7 +48,7 @@ export default function AnimatedLogo() {
       return (
          <Link
             href="/"
-            className="z-999 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform md:left-0 md:ml-4 md:-translate-x-0"
+            className="absolute left-1/2 top-1/2 z-[999] -translate-x-1/2 -translate-y-1/2 transform md:left-0 md:ml-4 md:-translate-x-0"
          >
             <div className="relative h-64 w-64">
                <Image
@@ -64,7 +64,7 @@ export default function AnimatedLogo() {
 }
 
 // Custom hook to detect if the window has scrolled past a threshold.
-function useScrolled(threshold: number = 999) {
+function useScrolled(threshold: number = 100) {
    const [scrolled, setScrolled] = useState(false);
 
    useEffect(() => {
