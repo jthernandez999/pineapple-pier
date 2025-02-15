096a5b4497778b43b7b9d8b58a95cc7dc2410732
Joe Hernandez <j.t.hernandez999@gmail.com>
2025-02-14 21:41:07 -0800

    updated collection page on mobile still wip

--- components/layout/product-grid-items.tsx
+++ components/layout/product-grid-items.tsx
@@ -78,7 +78,7 @@ function ProductGridItemsComponent({ products, groupHandle }: ProductGridItemsPr
                   />
                );
             })}
-         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3">
+         <div className="grid grid-cols-2 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3">
             {products.map((product) => (
                <Grid.Item key={product.handle} className="animate-fadeIn">
                   <Link
