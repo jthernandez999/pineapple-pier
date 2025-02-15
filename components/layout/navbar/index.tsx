f017671bdb961a4377cfe7704f7228b9f6d95ac7
Joe Hernandez <j.t.hernandez999@gmail.com>
2025-02-15 12:44:22 -0800

    wip nav

--- components/layout/navbar/index.tsx
+++ components/layout/navbar/index.tsx
@@ -53,7 +53,7 @@ export async function Navbar() {
 // Helper component for the logo area.
 function LogoArea() {
    return (
-      <div className="relative flex h-64 w-64 items-center">
+      <div className="flex items-center">
          <Link href="/" className="flex items-center gap-2">
             <AnimatedLogo />
             {/* <div className="ml-2 hidden text-sm font-medium uppercase md:block">{SITE_NAME}</div> */}
