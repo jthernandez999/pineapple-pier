1938d8ac087fdf89b2a502d69f04a4af22f0bbb4
Joe Hernandez <j.t.hernandez999@gmail.com>
2025-02-15 19:39:25 -0800

    wip logo

--- components/animated-logo.tsx
+++ components/animated-logo.tsx
@@ -25,7 +25,7 @@ export default function AnimatedLogo() {
       // the white logo overlay (when not scrolled) or the black logo (when scrolled).
       const containerClasses = scrolled
          ? 'left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 md:left-0 md:ml-4 md:-translate-x-0 '
-         : 'left-1/2 top-1/4 -translate-x-1/2 md:scale-[3.5] scale-[1.6] ';
+         : 'left-1/2 top-1/4 -translate-x-1/2 md:scale-[3.5] scale-[1.5] mx-auto px-auto';
       const imageSrc = scrolled ? '/dj_black_logo.png' : '/dj_white_logo.png';
 
       return (
