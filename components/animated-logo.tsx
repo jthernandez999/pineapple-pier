f0311fb93fa5927494dbefbfe4122b433c4a5326
Joe Hernandez <j.t.hernandez999@gmail.com>
2025-02-15 20:16:19 -0800

    wip nav bar

--- components/animated-logo.tsx
+++ components/animated-logo.tsx
@@ -20,126 +20,44 @@ export default function AnimatedLogo() {
    const isHome = pathname === '/';
    const scrolled = useScrolled(100);
 
-   if (isHome) {
-      // On the homepage, we render a single container that shows either
-      // the white logo overlay (when not scrolled) or the black logo (when scrolled).
-      const containerClasses = scrolled
-         ? 'left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 md:left-0 md:ml-4 md:-translate-x-0 '
-         : 'left-1/2 top-1/4 -translate-x-1/2 md:scale-[3.5] scale-[1.5] mx-auto px-auto';
-      const imageSrc = scrolled ? '/dj_black_logo.png' : '/dj_white_logo.png';
+   // When not scrolled on the homepage, we want the container to be:
+   // - On mobile: centered horizontally, positioned at about 25% from the top.
+   // - On desktop: the same, but the inner image scales to 2.5×.
+   const notScrolledClasses = 'left-1/2 top-1/4 -translate-x-1/2';
+   // When scrolled (or on non-home pages), we want:
+   // - On mobile: centered horizontally and vertically.
+   // - On desktop: aligned to the left with a margin.
+   const scrolledMobile = 'left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2';
+   const scrolledDesktop = 'md:left-0 md:ml-4 md:-translate-x-0';
 
-      return (
-         <Link
-            href="/"
-            className={`absolute z-30 transform transition-all duration-1000 ${containerClasses}`}
-         >
-            <div className="relative my-0 h-72 w-72 py-0 md:z-[10] md:h-96 md:w-96">
-               <Image
-                  src={imageSrc}
-                  alt="DJ Logo"
-                  fill
-                  style={{ objectFit: 'contain', objectPosition: 'center' }}
-                  unoptimized
-               />
-            </div>
-         </Link>
-      );
-   } else {
-      // On non-home pages, always display the black logo in the navbar.
-      return (
-         <Link
-            href="/"
-            className="absolute left-1/2 top-1/2 z-[10] -translate-x-1/2 -translate-y-1/2 transform md:left-0 md:ml-4 md:-translate-x-0"
-         >
-            <div className="relative h-72 w-72 md:h-80 md:w-80">
-               <Image
-                  src="/dj_black_logo.png"
-                  alt="DJ Black Logo"
-                  fill
-                  style={{ objectFit: 'contain', objectPosition: 'center' }}
-                  unoptimized
-               />
-            </div>
-         </Link>
-      );
-   }
+   // Pick the transform classes based on state:
+   const containerClasses =
+      isHome && !scrolled ? notScrolledClasses : `${scrolledMobile} ${scrolledDesktop}`;
+
+   // For scaling:
+   // When not scrolled on homepage, on desktop we scale up by 2.5x; otherwise scale is 1.
+   const scaleClasses = isHome && !scrolled ? 'md:scale-[4]' : 'scale-100';
+
+   // The image source: white logo when not scrolled on homepage; black logo otherwise.
+   const imageSrc = isHome && !scrolled ? '/dj_white_logo.png' : '/dj_black_logo.png';
+
+   // Use a 3000ms transition for transform changes.
+   const transitionClasses = 'transition-all duration-[2000ms] ease-in-out';
+
+   return (
+      <Link
+         href="/"
+         className={`absolute z-[999] ${containerClasses} ${scaleClasses} ${transitionClasses}`}
+      >
+         <div className="relative h-64 w-64 md:h-80 md:w-80">
+            <Image
+               src={imageSrc}
+               alt="DJ Logo"
+               fill
+               style={{ objectFit: 'contain', objectPosition: 'center' }}
+               unoptimized
+            />
+         </div>
+      </Link>
+   );
 }
-
-// 'use client';
-
-// import Image from 'next/image';
-// import Link from 'next/link';
-// import { usePathname } from 'next/navigation';
-// import { useEffect, useState } from 'react';
-
-// function useScrolled(threshold: number = 100) {
-//    const [scrolled, setScrolled] = useState(false);
-//    useEffect(() => {
-//       const onScroll = () => setScrolled(window.scrollY > threshold);
-//       window.addEventListener('scroll', onScroll);
-//       return () => window.removeEventListener('scroll', onScroll);
-//    }, [threshold]);
-//    return scrolled;
-// }
-
-// export default function AnimatedLogo() {
-//    const pathname = usePathname();
-//    const isHome = pathname === '/';
-//    const scrolled = useScrolled(100);
-
-//    if (isHome) {
-//       if (!scrolled) {
-//          // On the homepage when not scrolled: display the white logo overlay.
-//          // Render this inside a relatively positioned hero container.
-//          return (
-//             <div className="pointer-events-none absolute left-1/2 top-1/4 z-[10] h-[60vh] w-[80vw] -translate-x-1/2 transform md:w-[100vw]">
-//                <Image
-//                   src="/dj_white_logo.png"
-//                   alt="DJ White Logo"
-//                   fill
-//                   style={{ objectFit: 'contain', objectPosition: 'center' }}
-//                   priority
-//                   unoptimized
-//                />
-//             </div>
-//          );
-//       } else {
-//          // On the homepage when scrolled: display the black logo in the navbar.
-//          // On mobile, center the logo; on desktop, align it to the left.
-//          return (
-//             <Link
-//                href="/"
-//                className="absolute left-1/2 top-1/2 z-[10] ml-0 -translate-x-1/2 -translate-y-1/2 transform md:left-0 md:ml-4 md:-translate-x-0"
-//             >
-//                <div className="relative h-64 w-64">
-//                   <Image
-//                      src="/dj_black_logo.png"
-//                      alt="DJ Black Logo"
-//                      fill
-//                      style={{ objectFit: 'contain', objectPosition: 'center' }}
-//                      unoptimized
-//                   />
-//                </div>
-//             </Link>
-//          );
-//       }
-//    } else {
-//       // On non-home pages: always display the black logo in the navbar.
-//       return (
-//          <Link
-//             href="/"
-//             className="absolute left-1/2 top-1/2 z-[10] ml-0 -translate-x-1/2 -translate-y-1/2 transform md:left-0 md:ml-4 md:-translate-x-0"
-//          >
-//             <div className="relative h-64 w-64">
-//                <Image
-//                   src="/dj_black_logo.png"
-//                   alt="DJ Black Logo"
-//                   fill
-//                   style={{ objectFit: 'contain', objectPosition: 'center' }}
-//                   unoptimized
-//                />
-//             </div>
-//          </Link>
-//       );
-//    }
-// }
