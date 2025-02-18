7a48af98deff94ffef3638f858845a92ab159858
jthernandez999 <j.t.hernandez999@gmail.com>
2025-02-18 12:02:57 -0800

    updated assets

--- assets/index.tsx
+++ assets/index.tsx
@@ -1,9 +1,9 @@
 // assets.js
 export const banners = [
    {
-      image: 'https://cdn.shopify.com/s/files/1/1024/2207/files/mb1_lp.jpg?v=1738773383',
+      image: 'https://cdn.shopify.com/s/files/1/1024/2207/files/DSC_1702-2_2080.jpg?v=1739908611',
       mobileImage:
-         'https://cdn.shopify.com/s/files/1/1024/2207/files/mb1_lp_copyv2.jpg?v=1738954132',
+         'https://cdn.shopify.com/s/files/1/1024/2207/files/DSC_1702-2_1080.jpg?v=1739908611',
       title: 'Spring Refresh',
       description: 'SIMPLE, CHIC, EFFORTLESS',
       buttonText: 'Shop The Spring Collection',
@@ -14,25 +14,25 @@ export const banners = [
 
 export const collectionImages = [
    {
-      image: 'https://cdn.shopify.com/s/files/1/1024/2207/files/denim_52b557e5-beea-4fb3-972f-239787121d03.jpg?v=1738773584',
+      image: 'https://cdn.shopify.com/s/files/1/1024/2207/files/DSC_3020-2_1080.jpg?v=1739908794',
       title: 'Denim',
       //   description: 'SIMPLE, CHIC, EFFORTLESS',
       //   buttonText: 'Shop Now New Arrivals',
       buttonLink: '/collections/shop-all-denim'
    },
    {
-      image: 'https://cdn.shopify.com/s/files/1/1024/2207/files/tops_and_shirts.jpg?v=1738773584',
+      image: 'https://cdn.shopify.com/s/files/1/1024/2207/files/DSC_2894-2_1080.jpg?v=1739908789',
       title: 'Tops & Shirts',
       //   description: 'SIMPLE, CHIC, EFFORTLESS',
       //   buttonText: 'Shop Now New Arrivals',
       buttonLink: '/collections/shop-all-tops'
    },
    {
-      image: 'https://cdn.shopify.com/s/files/1/1024/2207/files/outerwear_88e6fd11-2791-4584-bda9-435ef523459b.jpg?v=1738773585',
-      title: 'Outerwear',
+      image: 'https://cdn.shopify.com/s/files/1/1024/2207/files/DSC_1797-2_1080.jpg?v=1739908726',
+      title: 'Shorts',
       //   description: 'SIMPLE, CHIC, EFFORTLESS',
       //   buttonText: 'Shop Now New Arrivals',
-      buttonLink: '/collections/basics'
+      buttonLink: '/collections/shop-all-shorts'
    }
 ];
 
