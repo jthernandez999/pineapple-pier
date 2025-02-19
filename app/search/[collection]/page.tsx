eeeaab7fd46ce3bb50fa79797b0a93f1656df87a
jthernandez999 <j.t.hernandez999@gmail.com>
2025-02-18 16:52:24 -0800

    wip

--- app/search/[collection]/page.tsx
+++ app/search/[collection]/page.tsx
@@ -44,7 +44,7 @@ export default async function CategoryPage(props: {
             <p className="py-3 text-lg">No products found in this collection</p>
          ) : (
             <>
-               <h1 className="border-b-2 border-gray-200 bg-gray-100 py-4 pl-4 text-left text-2xl font-bold uppercase tracking-wider">
+               <h1 className="border-b-2 border-gray-200 bg-gray-100 py-4 pl-4 text-left font-poppins text-xl uppercase tracking-wider">
                   {collectionTitle}
                </h1>
                <Grid className="container mx-auto w-full px-4">
