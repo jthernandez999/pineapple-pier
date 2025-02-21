085a13dd5e328fdcbefcf5e63adb2d4e31b6339a
jthernandez999 <j.t.hernandez999@gmail.com>
2025-02-21 15:54:52 -0800

    debug3

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,23 +1,14 @@
-import { headers } from 'next/headers';
-export const runtime = 'edge';
-export default async function AuthorizationPage() {
-   const headersList = headers();
-   const access = (await headersList).get('x-shop-access');
-   if (!access) {
-      console.log('ERROR: No access header');
-      throw new Error('No access header');
-   }
-   console.log('Authorize Access code header:', access);
-   if (access === 'denied') {
-      console.log('Access Denied for Auth');
-      throw new Error('No access allowed');
-   }
+import { LoginMessage } from 'components/auth/login-message';
+export const runtime = 'edge'; //this needs to be here on thie page. I don't know why
 
+export default async function LoginPage() {
    return (
       <>
          <div className="mx-auto max-w-screen-2xl px-4">
             <div className="flex flex-col rounded-lg border border-neutral-200 bg-white p-8 dark:border-neutral-800 dark:bg-black md:p-12 lg:flex-row lg:gap-8">
-               <div className="h-full w-full">Loading...</div>
+               <div className="h-full w-full">
+                  <LoginMessage />
+               </div>
             </div>
          </div>
       </>
