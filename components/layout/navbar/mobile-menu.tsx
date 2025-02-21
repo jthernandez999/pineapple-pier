37ca32800afc9a72b42679e888947f1c1dd11ea4
jthernandez999 <j.t.hernandez999@gmail.com>
2025-02-21 10:29:43 -0800

    updated nav bar on mobile

--- components/layout/navbar/mobile-menu.tsx
+++ components/layout/navbar/mobile-menu.tsx
@@ -1,100 +1,152 @@
 'use client';
 
 import { Dialog, Transition } from '@headlessui/react';
+import { Bars3Icon, ChevronDownIcon, XMarkIcon } from '@heroicons/react/24/outline';
+import { Menu } from 'lib/shopify/types';
 import Link from 'next/link';
 import { usePathname, useSearchParams } from 'next/navigation';
 import { Fragment, Suspense, useEffect, useState } from 'react';
-
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
-import { Menu } from 'lib/shopify/types';
 import Search, { SearchSkeleton } from './search';
 
 export default function MobileMenu({ menu }: { menu: Menu[] }) {
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
-  const [isOpen, setIsOpen] = useState(false);
-  const openMobileMenu = () => setIsOpen(true);
-  const closeMobileMenu = () => setIsOpen(false);
+   const pathname = usePathname();
+   const searchParams = useSearchParams();
+   const [isOpen, setIsOpen] = useState(false);
+   // openSubMenu will store the title of the menu item whose submenu is open.
+   const [openSubMenu, setOpenSubMenu] = useState<string | null>(null);
 
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth > 768) {
-        setIsOpen(false);
+   const openMobileMenu = () => setIsOpen(true);
+   const closeMobileMenu = () => {
+      setIsOpen(false);
+      setOpenSubMenu(null);
+   };
+
+   // Close mobile menu on resize if the viewport exceeds mobile size.
+   useEffect(() => {
+      const handleResize = () => {
+         if (window.innerWidth > 768) {
+            closeMobileMenu();
+         }
+      };
+      window.addEventListener('resize', handleResize);
+      return () => window.removeEventListener('resize', handleResize);
+   }, []);
+
+   // Close mobile menu if the route changes.
+   useEffect(() => {
+      closeMobileMenu();
+   }, [pathname, searchParams]);
+
+   // Handle clicking on a top-level menu item.
+   const handleTopLevelClick = (item: Menu) => {
+      // If there are sub-items, toggle the submenu.
+      if (item.items && item.items.length > 0) {
+         setOpenSubMenu((prev) => (prev === item.title ? null : item.title));
+      } else {
+         // Otherwise, close the menu and navigate.
+         closeMobileMenu();
       }
-    };
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, [isOpen]);
+   };
 
-  useEffect(() => {
-    setIsOpen(false);
-  }, [pathname, searchParams]);
-
-  return (
-    <>
-      <button
-        onClick={openMobileMenu}
-        aria-label="Open mobile menu"
-        className="flex h-11 w-11 items-center justify-center rounded-md border border-neutral-200 text-black transition-colors md:hidden dark:border-neutral-700 dark:text-white"
-      >
-        <Bars3Icon className="h-4" />
-      </button>
-      <Transition show={isOpen}>
-        <Dialog onClose={closeMobileMenu} className="relative z-50">
-          <Transition.Child
-            as={Fragment}
-            enter="transition-all ease-in-out duration-300"
-            enterFrom="opacity-0 backdrop-blur-none"
-            enterTo="opacity-100 backdrop-blur-[.5px]"
-            leave="transition-all ease-in-out duration-200"
-            leaveFrom="opacity-100 backdrop-blur-[.5px]"
-            leaveTo="opacity-0 backdrop-blur-none"
-          >
-            <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
-          </Transition.Child>
-          <Transition.Child
-            as={Fragment}
-            enter="transition-all ease-in-out duration-300"
-            enterFrom="translate-x-[-100%]"
-            enterTo="translate-x-0"
-            leave="transition-all ease-in-out duration-200"
-            leaveFrom="translate-x-0"
-            leaveTo="translate-x-[-100%]"
-          >
-            <Dialog.Panel className="fixed bottom-0 left-0 right-0 top-0 flex h-full w-full flex-col bg-white pb-6 dark:bg-black">
-              <div className="p-4">
-                <button
-                  className="mb-4 flex h-11 w-11 items-center justify-center rounded-md border border-neutral-200 text-black transition-colors dark:border-neutral-700 dark:text-white"
-                  onClick={closeMobileMenu}
-                  aria-label="Close mobile menu"
-                >
-                  <XMarkIcon className="h-6" />
-                </button>
-
-                <div className="mb-4 w-full">
-                  <Suspense fallback={<SearchSkeleton />}>
-                    <Search />
-                  </Suspense>
-                </div>
-                {menu.length ? (
-                  <ul className="flex w-full flex-col">
-                    {menu.map((item: Menu) => (
-                      <li
-                        className="py-2 text-xl text-black transition-colors hover:text-neutral-500 dark:text-white"
-                        key={item.title}
-                      >
-                        <Link href={item.path} prefetch={true} onClick={closeMobileMenu}>
-                          {item.title}
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
-                ) : null}
-              </div>
-            </Dialog.Panel>
-          </Transition.Child>
-        </Dialog>
-      </Transition>
-    </>
-  );
+   return (
+      <>
+         <button
+            onClick={openMobileMenu}
+            aria-label="Open mobile menu"
+            className="flex h-11 w-11 items-center justify-center rounded-md border border-neutral-200 text-black transition-colors dark:border-neutral-700 dark:text-white md:hidden"
+         >
+            <Bars3Icon className="h-4" />
+         </button>
+         <Transition show={isOpen}>
+            <Dialog onClose={closeMobileMenu} className="relative z-50">
+               <Transition.Child
+                  as={Fragment}
+                  enter="transition-all ease-in-out duration-300"
+                  enterFrom="opacity-0 backdrop-blur-none"
+                  enterTo="opacity-100 backdrop-blur-[.5px]"
+                  leave="transition-all ease-in-out duration-200"
+                  leaveFrom="opacity-100 backdrop-blur-[.5px]"
+                  leaveTo="opacity-0 backdrop-blur-none"
+               >
+                  <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
+               </Transition.Child>
+               <Transition.Child
+                  as={Fragment}
+                  enter="transition-all ease-in-out duration-300"
+                  enterFrom="translate-x-[-100%]"
+                  enterTo="translate-x-0"
+                  leave="transition-all ease-in-out duration-200"
+                  leaveFrom="translate-x-0"
+                  leaveTo="translate-x-[-100%]"
+               >
+                  <Dialog.Panel className="fixed inset-0 flex h-full w-full flex-col bg-white pb-6 dark:bg-black">
+                     <div className="p-4">
+                        <button
+                           className="mb-4 flex h-11 w-11 items-center justify-center rounded-md border border-neutral-200 text-black transition-colors dark:border-neutral-700 dark:text-white"
+                           onClick={closeMobileMenu}
+                           aria-label="Close mobile menu"
+                        >
+                           <XMarkIcon className="h-6" />
+                        </button>
+                        <div className="mb-4 w-full">
+                           <Suspense fallback={<SearchSkeleton />}>
+                              <Search />
+                           </Suspense>
+                        </div>
+                        {menu.length ? (
+                           <ul className="flex w-full flex-col">
+                              {menu.map((item: Menu) => (
+                                 <li
+                                    key={item.title}
+                                    className="py-2 text-xl text-black transition-colors hover:text-neutral-500 dark:text-white"
+                                 >
+                                    {item.items && item.items.length > 0 ? (
+                                       <>
+                                          <button
+                                             onClick={() => handleTopLevelClick(item)}
+                                             className="flex w-full items-center justify-between"
+                                          >
+                                             <span>{item.title}</span>
+                                             <ChevronDownIcon
+                                                className={`h-5 w-5 transition-transform ${
+                                                   openSubMenu === item.title ? 'rotate-180' : ''
+                                                }`}
+                                             />
+                                          </button>
+                                          {openSubMenu === item.title && (
+                                             <ul className="ml-4 mt-2 flex flex-col space-y-2">
+                                                {item.items.map((subItem) => (
+                                                   <li key={subItem.title}>
+                                                      <Link
+                                                         href={subItem.path}
+                                                         onClick={closeMobileMenu}
+                                                         className="text-md block font-semibold text-gray-700 hover:text-gray-900"
+                                                      >
+                                                         {subItem.title}
+                                                      </Link>
+                                                   </li>
+                                                ))}
+                                             </ul>
+                                          )}
+                                       </>
+                                    ) : (
+                                       <Link
+                                          href={item.path}
+                                          prefetch={true}
+                                          onClick={closeMobileMenu}
+                                       >
+                                          {item.title}
+                                       </Link>
+                                    )}
+                                 </li>
+                              ))}
+                           </ul>
+                        ) : null}
+                     </div>
+                  </Dialog.Panel>
+               </Transition.Child>
+            </Dialog>
+         </Transition>
+      </>
+   );
 }
