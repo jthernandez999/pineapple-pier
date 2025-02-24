0354885d2ecd18b94dd96e44c7f41bb49eed607d
jthernandez999 <j.t.hernandez999@gmail.com>
2025-02-24 10:04:53 -0800

    created order cards in account-orders-histor.tsx component

--- components/account/account-orders-history.tsx
+++ components/account/account-orders-history.tsx
@@ -34,8 +34,36 @@ function Orders({ orders }: OrderCardsProps) {
    return (
       <ul className="false grid grid-flow-row grid-cols-1 gap-2 gap-y-6 sm:grid-cols-3 md:gap-4 lg:gap-6">
          {orders.map((order: any) => (
-            <li key={order.node.id}>{order.node.number}</li>
+            <li key={order.node.id}>
+               {order.node.number}
+               <OrderCard order={order} />
+            </li>
          ))}
       </ul>
+   );
+}
+
+function OrderCard({ order }: { order: any }) {
+   return (
+      <div>
+         <div className="flex justify-between">
+            <div>
+               <div className="text-sm font-bold">Order Number</div>
+               <div className="text-sm">{order.node.number}</div>
+            </div>
+            <div>
+               <div className="text-sm font-bold">Order Date</div>
+               <div className="text-sm">{order.node.created}</div>
+            </div>
+         </div>
+         <div>
+            <div className="text-sm font-bold">Items</div>
+            <div className="text-sm">{order.node.items.length}</div>
+         </div>
+         <div>
+            <div className="text-sm font-bold">Total</div>
+            <div className="text-sm">{order.node.total}</div>
+         </div>
+      </div>
    );
 }
