296eaa9f39ee21d49ecc1f3f062a42c64b83f377
Joe Hernandez <j.t.hernandez999@gmail.com>
2025-03-01 17:51:37 -0800

    logging getcollectionsproduct

--- lib/shopify/index.ts
+++ lib/shopify/index.ts
@@ -348,6 +348,7 @@ export async function getCollectionProducts({
    }
    const productsData = res.body.data.collection.products as any; // cast to any to access pageInfo
    console.log('Raw collection products response:::', res.body.data.collection.products);
+   console.log(productsData.edges[0].node);
 
    const pageInfo = productsData.pageInfo as { hasNextPage: boolean; endCursor: string | null };
 
