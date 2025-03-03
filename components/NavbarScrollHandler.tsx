679172b180b1aeed61d67ea817d72ffd2c77630f
jthernandez999 <j.t.hernandez999@gmail.com>
2025-03-03 14:00:47 -0800

    updated nav bug when going to prev page

--- components/NavbarScrollHandler.tsx
+++ components/NavbarScrollHandler.tsx
@@ -6,48 +6,82 @@ const NavbarScrollHandler = () => {
    const navbarHeight = 104; // Adjust to your navbar's height in pixels
 
    useEffect(() => {
+      // Check if the current path is the homepage, collection page or product page
+      const isHomepage = window.location.pathname === '/';
+      const isCollectionPage = window.location.pathname.startsWith('/collections/');
+      const isProductPage = window.location.pathname.startsWith('/product/');
       const navElement = document.querySelector('nav'); // Select the nav element
+      const desktopMenuId = document.getElementById('desktop-menu'); // ID of the desktop menu
 
       // Function to handle scroll and change classes
       const handleScroll = () => {
          const navbarHeight = 104; // Adjust based on your navbar's height
-         const navElement = document.querySelector('nav'); // Select the nav element
-         const desktopMenuId = document.getElementById('desktop-menu'); // ID of the desktop menu
-
          if (navElement && desktopMenuId && window.location.pathname === '/') {
-            // Only apply scroll behavior on homepage
-            if (window.scrollY > navbarHeight) {
-               // When scrolled past the threshold, add sticky and remove absolute
-               navElement.classList.remove('absolute', 'lg:top-20', 'top-10', 'bg-white-opacity-0');
-               desktopMenuId.classList.remove('md:hidden');
+            if (isHomepage && !isCollectionPage && !isProductPage) {
+               // Only apply scroll behavior on homepage
+               if (window.scrollY > navbarHeight) {
+                  // When scrolled past the threshold, add sticky and remove absolute
+                  navElement.classList.remove(
+                     'absolute',
+                     'lg:top-20',
+                     'top-10',
+                     'bg-white-opacity-0'
+                  );
+                  desktopMenuId.classList.remove('md:hidden');
+                  navElement.classList.add(
+                     'sticky',
+                     'top-0',
+                     'lg:top-0',
+                     'shadow-md',
+                     'bg-white',
+                     'bg-opacity-100', // Full opacity when sticky
+                     'transition-all',
+                     'duration-[2000ms]',
+                     'ease-in-out'
+                  );
+                  desktopMenuId.classList.add('md:block');
+               } else {
+                  // When scrolled back up, add absolute and remove sticky
+                  navElement.classList.add(
+                     'absolute',
+                     'lg:top-20',
+                     'top-10',
+                     'bg-transparent',
+                     'bg-opacity-0', // Transparent background when not sticky
+                     'transition-all',
+                     'duration-[2000ms]',
+                     'ease-in-out',
+                     `bottom-[-${navbarHeight}px]`
+                  );
+                  desktopMenuId.classList.add('md:hidden');
+                  desktopMenuId.classList.remove('md:block');
+                  navElement.classList.remove(
+                     'sticky',
+                     'bg-white',
+                     'shadow-md',
+                     'lg:top-0',
+                     'top-0'
+                  );
+               }
+            } else {
+               // For all other pages, maintain sticky navbar without transition
                navElement.classList.add(
                   'sticky',
                   'top-0',
-                  'lg:top-0',
-                  'shadow-md',
+                  'z-30',
                   'bg-white',
-                  'bg-opacity-100', // Full opacity when sticky
-                  'transition-all',
-                  'duration-[2000ms]',
-                  'ease-in-out'
+                  'bg-opacity-100',
+                  'transition-none'
                );
-               desktopMenuId.classList.add('md:block');
-            } else {
-               // When scrolled back up, add absolute and remove sticky
-               navElement.classList.add(
+               navElement.classList.remove(
                   'absolute',
                   'lg:top-20',
                   'top-10',
                   'bg-transparent',
-                  'bg-opacity-0', // Transparent background when not sticky
-                  'transition-all',
-                  'duration-[2000ms]',
-                  'ease-in-out',
-                  `bottom-[-${navbarHeight}px]`
+                  'bg-opacity-0'
                );
-               desktopMenuId.classList.add('md:hidden');
-               desktopMenuId.classList.remove('md:block');
-               navElement.classList.remove('sticky', 'bg-white', 'shadow-md', 'lg:top-0', 'top-0');
+               desktopMenuId.classList.remove('md:hidden');
+               desktopMenuId.classList.add('md:block');
             }
          }
       };
