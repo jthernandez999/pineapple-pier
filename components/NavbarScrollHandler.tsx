6c879159128b59c5d82d331d135580f5985b4311
jthernandez999 <j.t.hernandez999@gmail.com>
2025-03-03 09:21:47 -0800

    updated nav bar transparency transition smoothness

--- components/NavbarScrollHandler.tsx
+++ components/NavbarScrollHandler.tsx
@@ -12,12 +12,14 @@ const NavbarScrollHandler = () => {
       const handleScroll = () => {
          const navbarHeight = 100; // Adjust based on your navbar's height
          const navElement = document.querySelector('nav'); // Select the nav element
+         const desktopMenuId = document.getElementById('desktop-menu'); // ID of the desktop menu
 
-         if (navElement && window.location.pathname === '/') {
+         if (navElement && desktopMenuId && window.location.pathname === '/') {
             // Only apply scroll behavior on homepage
             if (window.scrollY > navbarHeight) {
                // When scrolled past the threshold, add sticky and remove absolute
                navElement.classList.remove('absolute', 'lg:top-20', 'top-10', 'bg-white-opacity-0');
+               desktopMenuId.classList.remove('md:hidden');
                navElement.classList.add(
                   'sticky',
                   'top-0',
@@ -29,6 +31,7 @@ const NavbarScrollHandler = () => {
                   'duration-[2000ms]',
                   'ease-in-out'
                );
+               desktopMenuId.classList.add('md:block');
             } else {
                // When scrolled back up, add absolute and remove sticky
                navElement.classList.add(
@@ -42,6 +45,8 @@ const NavbarScrollHandler = () => {
                   'ease-in-out',
                   `bottom-[-${navbarHeight}px]`
                );
+               desktopMenuId.classList.add('md:hidden');
+               desktopMenuId.classList.remove('md:block');
                navElement.classList.remove('sticky', 'bg-white', 'shadow-md', 'lg:top-0', 'top-0');
             }
          }
