d5eef9fd49c52961580ee3876c5dc2c7478f9270
jthernandez999 <j.t.hernandez999@gmail.com>
2025-03-03 09:06:36 -0800

    updated smooth transition onn navbar

--- components/NavbarScrollHandler.tsx
+++ components/NavbarScrollHandler.tsx
@@ -10,25 +10,36 @@ const NavbarScrollHandler = () => {
 
       // Function to handle scroll and change classes
       const handleScroll = () => {
+         const navbarHeight = 100; // Adjust based on your navbar's height
+         const navElement = document.querySelector('nav'); // Select the nav element
+
          if (navElement && window.location.pathname === '/') {
             // Only apply scroll behavior on homepage
             if (window.scrollY > navbarHeight) {
                // When scrolled past the threshold, add sticky and remove absolute
-               navElement.classList.remove(
-                  'absolute',
-                  'lg:top-20',
-                  'bg-white-opacity-0',
-                  'top-10'
-                  // `bottom-[-${navbarHeight}px]`
+               navElement.classList.remove('absolute', 'lg:top-20', 'top-10', 'bg-white-opacity-0');
+               navElement.classList.add(
+                  'sticky',
+                  'top-0',
+                  'lg:top-0',
+                  'shadow-md',
+                  'bg-white',
+                  'bg-opacity-100', // Full opacity when sticky
+                  'transition-all',
+                  'duration-[2000ms]',
+                  'ease-in-out'
                );
-               navElement.classList.add('sticky', 'top-0', 'lg:top-0', 'shadow-md', 'bg-white');
             } else {
                // When scrolled back up, add absolute and remove sticky
                navElement.classList.add(
                   'absolute',
                   'lg:top-20',
                   'top-10',
-                  'bg-white-opacity-0',
+                  'bg-transparent',
+                  'bg-opacity-0', // Transparent background when not sticky
+                  'transition-all',
+                  'duration-[2000ms]',
+                  'ease-in-out',
                   `bottom-[-${navbarHeight}px]`
                );
                navElement.classList.remove('sticky', 'bg-white', 'shadow-md', 'lg:top-0', 'top-0');
