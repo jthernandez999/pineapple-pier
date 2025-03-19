5896c623d69acc4ace298000d44aec9f27174805
jthernandez999 <j.t.hernandez999@gmail.com>
2025-03-19 13:21:25 -0700

    updated sort filter on collections search page

--- app/search/layout.tsx
+++ app/search/layout.tsx
@@ -1,6 +1,7 @@
 import Footer from 'components/layout/footer';
 import Collections from 'components/layout/search/collections';
-// import { FilterList } from '../../components/filters/filter-list';
+import FilterList from 'components/layout/search/filter';
+import { sorting } from 'lib/constants';
 import ChildrenWrapper from './children-wrapper';
 export default function SearchLayout({ children }: { children: React.ReactNode }) {
    return (
@@ -12,9 +13,9 @@ export default function SearchLayout({ children }: { children: React.ReactNode }
             <div className="order-last min-h-screen w-full md:order-none">
                <ChildrenWrapper>{children}</ChildrenWrapper>
             </div>
-            {/* <div className="order-none flex-none md:order-last md:w-[150px]"> */}
-            {/* <FilterList title="Sort by" /> */}
-            {/* </div> */}
+            <div className="order-none flex-none md:order-last md:w-[150px]">
+               <FilterList list={sorting} title="Sort by" />
+            </div>
          </div>
          <Footer />
       </>
--- components/layout/search/filter/index.tsx
+++ components/layout/search/filter/index.tsx
@@ -1,5 +1,4 @@
 import { SortFilterItem } from 'lib/constants';
-import { Suspense } from 'react';
 import FilterItemDropdown from './dropdown';
 import { FilterItem } from './item';
 
@@ -7,35 +6,31 @@ export type ListItem = SortFilterItem | PathFilterItem;
 export type PathFilterItem = { title: string; path: string };
 
 function FilterItemList({ list }: { list: ListItem[] }) {
-  return (
-    <>
-      {list.map((item: ListItem, i) => (
-        <FilterItem key={i} item={item} />
-      ))}
-    </>
-  );
+   return (
+      <>
+         {list.map((item: ListItem, i) => (
+            <FilterItem key={i} item={item} />
+         ))}
+      </>
+   );
 }
 
 export default function FilterList({ list, title }: { list: ListItem[]; title?: string }) {
-  return (
-    <>
-      <nav>
-        {title ? (
-          <h3 className="hidden text-xs text-neutral-500 md:block dark:text-neutral-400">
-            {title}
-          </h3>
-        ) : null}
-        <ul className="hidden md:block">
-          <Suspense fallback={null}>
-            <FilterItemList list={list} />
-          </Suspense>
-        </ul>
-        <ul className="md:hidden">
-          <Suspense fallback={null}>
-            <FilterItemDropdown list={list} />
-          </Suspense>
-        </ul>
-      </nav>
-    </>
-  );
+   return (
+      <>
+         <nav>
+            {title ? (
+               <h3 className="hidden text-xs text-neutral-500 dark:text-neutral-400 md:block">
+                  {title}
+               </h3>
+            ) : null}
+            <ul className="hidden md:block">
+               <FilterItemList list={list} />
+            </ul>
+            <ul className="md:hidden">
+               <FilterItemDropdown list={list} />
+            </ul>
+         </nav>
+      </>
+   );
 }
