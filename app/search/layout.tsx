70f98d2d2d207478415ced7c3213486160e9cf30
jthernandez999 <j.t.hernandez999@gmail.com>
2025-03-19 13:48:01 -0700

    reverted sort to original

--- app/search/layout.tsx
+++ app/search/layout.tsx
@@ -3,10 +3,11 @@ import Collections from 'components/layout/search/collections';
 import FilterList from 'components/layout/search/filter';
 import { sorting } from 'lib/constants';
 import ChildrenWrapper from './children-wrapper';
+
 export default function SearchLayout({ children }: { children: React.ReactNode }) {
    return (
       <>
-         <div className="max-w-screen-3xl mx-auto flex flex-col gap-6 px-0 pb-4 text-black dark:text-white md:flex-row md:gap-4 md:px-4">
+         <div className="max-w-screen-full mx-auto flex flex-col gap-8 px-4 pb-4 text-black dark:text-white md:flex-row">
             <div className="order-first w-full flex-none md:max-w-[150px]">
                <Collections />
             </div>
