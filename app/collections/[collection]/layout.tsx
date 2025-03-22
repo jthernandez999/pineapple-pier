e5b362a7b3dedb25ee7b17ced828f43e21bdca74
jthernandez999 <j.t.hernandez999@gmail.com>
2025-03-21 17:00:11 -0700

    updated collections layout page

--- app/collections/[collection]/layout.tsx
+++ app/collections/[collection]/layout.tsx
@@ -1,20 +1,17 @@
 // app/collections/[collection]/layout.tsx
 // import ClientDynamicSizeFilter from 'components/filters/ClientDynamicSizeFilter';
+// app/collections/[collection]/layout.tsx
 import Footer from 'components/layout/footer';
 import FilterList from 'components/layout/search/filter';
 import { sorting } from 'lib/constants';
-import { getAvailableSizes } from 'lib/helpers/getAvailableSizes';
-import type { Product } from 'lib/shopify/types';
 import React from 'react';
 
-interface CollectionsLayoutProps {
-   products: Product[];
+export interface CollectionsLayoutProps {
    children: React.ReactNode;
+   params: { collection: string };
 }
 
-function CollectionsLayout({ products, children }: CollectionsLayoutProps) {
-   const { letters, numerics } = getAvailableSizes(products);
-
+function CollectionsLayout({ children, params }: CollectionsLayoutProps) {
    return (
       <>
          <div className="max-w-screen-full mx-auto flex flex-col gap-8 px-[0.10rem] pb-4 pt-4 text-black dark:text-white md:flex-row md:px-[0.20rem] xl:p-6">
