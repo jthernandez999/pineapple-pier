5c99cf00b6704b71d2725cd204fdf3b908b341d5
jthernandez999 <j.t.hernandez999@gmail.com>
2025-03-26 11:05:10 -0700

    updated logo left position

--- components/animated-logo.tsx
+++ components/animated-logo.tsx
@@ -38,7 +38,7 @@ export default function AnimatedLogo() {
    // - On desktop: aligned to the left with a margin.
    const scrolledMobile = 'left-1/2 top-1/2 -translate-x-1/2 -translate-y-1 md:-translate-y-1/2';
    const scrolledDesktop =
-      'md:hidden lg:hidden xl:block xl:left-[4rem] xl:ml-4 xl:-translate-x-0 xl:top-1/2 xl:-translate-y-1/2  scale-[1.3] xl:scale-[.5]  xl:scale-[.25] xl:-translate-x-[12rem] xl:scale-[.38] xl:-translate-x-[9rem]  2xl:scale-[.5] 2xl:-translate-x-[7rem] 2xl:ml-0 2xl:pl-1';
+      'md:hidden lg:hidden xl:block xl:left-[4rem] xl:ml-4 xl:-translate-x-0 xl:top-1/2 xl:-translate-y-1/2  scale-[1.3] xl:scale-[.5]  xl:scale-[.25] xl:-translate-x-[12rem] xl:scale-[.38] xl:-translate-x-[9rem]  2xl:scale-[.5] 2xl:-translate-x-[10.5rem] 2xl:ml-0 2xl:pl-0';
 
    // Pick the transform classes based on state:
    const containerClasses =
