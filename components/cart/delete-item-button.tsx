754a7f43e8b6546f8977868b796447e0ea7a01ab
jthernandez999 <j.t.hernandez999@gmail.com>
2025-04-01 14:12:33 -0700

    updated route

--- components/cart/delete-item-button.tsx
+++ components/cart/delete-item-button.tsx
@@ -6,33 +6,33 @@ import type { CartItem } from 'lib/shopify/types';
 import { useActionState } from 'react';
 
 export function DeleteItemButton({
-  item,
-  optimisticUpdate
+   item,
+   optimisticUpdate
 }: {
-  item: CartItem;
-  optimisticUpdate: any;
+   item: CartItem;
+   optimisticUpdate: any;
 }) {
-  const [message, formAction] = useActionState(removeItem, null);
-  const merchandiseId = item.merchandise.id;
-  const actionWithVariant = formAction.bind(null, merchandiseId);
+   const [message, formAction] = useActionState(removeItem, null);
+   const merchandiseId = item.merchandise.id;
+   const actionWithVariant = formAction.bind(null, merchandiseId);
 
-  return (
-    <form
-      action={async () => {
-        optimisticUpdate(merchandiseId, 'delete');
-        await actionWithVariant();
-      }}
-    >
-      <button
-        type="submit"
-        aria-label="Remove cart item"
-        className="flex h-[24px] w-[24px] items-center justify-center rounded-full bg-neutral-500"
+   return (
+      <form
+         action={async () => {
+            optimisticUpdate(merchandiseId, 'delete');
+            await actionWithVariant();
+         }}
       >
-        <XMarkIcon className="mx-[1px] h-4 w-4 text-white dark:text-black" />
-      </button>
-      <p aria-live="polite" className="sr-only" role="status">
-        {message}
-      </p>
-    </form>
-  );
+         <button
+            type="submit"
+            aria-label="Remove cart item"
+            className="flex h-[24px] w-[24px] items-center justify-center rounded-full bg-neutral-500"
+         >
+            <XMarkIcon className="mx-[1px] h-4 w-4 text-white dark:text-black" />
+         </button>
+         <p aria-live="polite" className="sr-only" role="status">
+            {message ?? ''}
+         </p>
+      </form>
+   );
 }
