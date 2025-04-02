60e20a328f1f29211a3ee2686e839a778d80aa5f
jthernandez999 <j.t.hernandez999@gmail.com>
2025-04-02 11:13:38 -0700

    removed address from account profile

--- components/account/account-dashboard.tsx
+++ components/account/account-dashboard.tsx
@@ -206,7 +206,7 @@ export default function AccountDashboard({
             </div>
             <nav>
                <ul>
-                  {['welcome', 'orders', 'personal', 'address', 'manage'].map((menu) => (
+                  {['welcome', 'orders', 'personal', 'manage'].map((menu) => (
                      <li key={menu} className="mb-1">
                         <button
                            onClick={() => setActiveMenu(menu)}
