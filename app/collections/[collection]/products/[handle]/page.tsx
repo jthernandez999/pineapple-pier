3548209692f3534174c5207932f4f8177c0f376d
Joe Hernandez <j.t.hernandez999@gmail.com>
2025-04-02 19:23:05 -0700

    debugging

--- app/collections/[collection]/products/[handle]/page.tsx
+++ app/collections/[collection]/products/[handle]/page.tsx
@@ -1,14 +1,6 @@
 // app/collections/[collection]/products/[handle]/page.tsx
 import ProductPage from '../../../../products/[handle]/page';
 
-export default function CollectionProductPage({
-   params
-}: {
-   params: { collection: string; handle: string };
-}) {
-   return (
-      <ProductPage
-         params={Promise.resolve(params) as Promise<{ collection: string; handle: string }>}
-      />
-   );
+export default function CollectionProductPage(props: any) {
+   return <ProductPage params={Promise.resolve(props.params)} />;
 }
