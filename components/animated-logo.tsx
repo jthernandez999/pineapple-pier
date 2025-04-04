d60e9ca642f374362ebcf6855b00b36221699166
jthernandez999 <j.t.hernandez999@gmail.com>
2025-04-04 12:04:44 -0700

    added priority to logo;

--- components/animated-logo.tsx
+++ components/animated-logo.tsx
@@ -68,6 +68,7 @@ export default function AnimatedLogo() {
             // style={{ objectFit: 'contain', objectPosition: 'center' }}
             className="object-contain, object-center"
             unoptimized
+            priority
          />
       </div>
    );
